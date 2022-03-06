4171cf2b53123d5e0c1d642c352b5ba75cd0d832
AnastasiaCymbalyuk <n.wolk@mail.ru>
2022-03-06 13:31:39 +0300

    async(fixed)

--- src/js/__tests__/app.test.js
+++ src/js/__tests__/app.test.js
@@ -1,7 +1,28 @@
-import GameSavingLoader from '../loader';
+import GameSavingLoader, { GameSaving } from '../loader';
 
-it('test', async () => {
-  const save = await GameSavingLoader.load();
+// eslint-disable-next-line arrow-body-style
+it('test', () => {
+  return GameSavingLoader.load().then((save) => {
+    expect(save).toEqual({
+      id: 9,
+      created: 1546300800,
+      userInfo: {
+        id: 1,
+        name: 'Hitman',
+        level: 10,
+        points: 2000,
+      },
+    });
+  }, (error) => error);
+});
+
+it('test obj', () => {
+  const save = new GameSaving(9, 1546300800, {
+    id: 1,
+    name: 'Hitman',
+    level: 10,
+    points: 2000,
+  });
   expect(save).toEqual({
     id: 9,
     created: 1546300800,
--- src/js/loader.js
+++ src/js/loader.js
@@ -1,10 +1,19 @@
+// eslint-disable-next-line max-classes-per-file
 import read from './reader';
 import json from './parser';
+
+export class GameSaving {
+  constructor(id, created, userInfo) {
+    this.id = id;
+    this.created = created;
+    this.userInfo = userInfo;
+  }
+}
 
 export default class GameSavingLoader {
   static load() {
     return read()
-      .then((result) => (json(result)))
-      .then((save) => (JSON.parse(save)));
+      .then((result) => json(result))
+      .then((save) => new GameSaving(JSON.parse(save)));
   }
 }
